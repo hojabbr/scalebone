a5dc4730ed5be6ffe95e4311799e2fb985c81831
Hossein Jabbari <hossein.jabbari@actineo.de>
2022-04-10 13:09:36 +0200

    feat: Fix app.php Kernels namespaces

--- src/App/ConsoleKernel.php
+++ src/App/ConsoleKernel.php
@@ -7,26 +7,17 @@ use Illuminate\Foundation\Console\Kernel;
 
 class ConsoleKernel extends Kernel
 {
-    /**
-     * Define the application's command schedule.
-     *
-     * @param \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
-     */
+    protected $commands = [
+        //
+    ];
+
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
-        $this->load(__DIR__ . '/Commands');
-
-        require base_path('routes/console.php');
+        $this->load(__DIR__.'/Commands');
     }
 }
